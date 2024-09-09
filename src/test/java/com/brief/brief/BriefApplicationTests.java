51485dbd37e3211af946a1c46b554252275984bb
Gerard-B-dev <gerardchank@gmail.com>
2024-09-09 11:00:05 +0200

    first commit

---
+++ src/test/java/com/brief/brief/BriefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brief.brief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
